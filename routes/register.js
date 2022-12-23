ce7a17e6ad169225e13f6f148136e6115526c8fa
wideputin117 <takermanish7@gmail.com>
2022-12-24 02:00:46 +0530

    comments added

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,8 @@
 const express = require('express')
 const { route } = require('../app')
 const router = express.Router()
-
+// to access this localhost:3000/
+// this will render the index.mustache
 router.post('/user', (req, res)=>{
     const body = req.body
 
